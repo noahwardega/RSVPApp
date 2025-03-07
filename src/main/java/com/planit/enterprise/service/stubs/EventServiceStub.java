1da12a5c9027101e626b7004ffe6445a493c4183
noahwardega <wardenoah123@gmail.com>
2025-03-06 19:28:24 -0500

    started tests creation

--- src/main/java/com/planit/enterprise/service/stubs/EventServiceStub.java
+++ src/main/java/com/planit/enterprise/service/stubs/EventServiceStub.java
@@ -1,4 +1,33 @@
 package com.planit.enterprise.service.stubs;
 
-public class EventServiceStub {
+import com.planit.enterprise.dto.EventDTO;
+import com.planit.enterprise.service.interfaces.IEventService;
+
+import java.util.List;
+
+public class EventServiceStub implements IEventService {
+    @Override
+    public void createEvent(EventDTO event) {
+
+    }
+
+    @Override
+    public List<EventDTO> getAllEvents() {
+        return List.of();
+    }
+
+    @Override
+    public EventDTO getEventById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateEvent(int eventId, EventDTO event) {
+
+    }
+
+    @Override
+    public void deleteEvent(int eventId) {
+
+    }
 }
--- src/test/java/com/planit/enterprise/PlanItApplicationTests.java
+++ src/test/java/com/planit/enterprise/PlanItApplicationTests.java
@@ -1,10 +1,17 @@
 package com.planit.enterprise;
 
+import com.planit.enterprise.dto.EventDTO;
+import com.planit.enterprise.service.interfaces.IEventService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PlanItApplicationTests {
+
+    @Autowired
+    private IEventService eventService;
+    private EventDTO event;
 
     @Test
     void contextLoads() {
